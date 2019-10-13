48422d8e0c54a75266942326658414236beef41e
Yunus Emre KAŞ <y.emrekas@outlook.com>
2019-10-14 00:00:15 +0300

    Veritabanı nesneleri oluşturuldu. Projeye SQLite eklendi.

---
+++ JWTSample/JWTSample/Models/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace JWTSample.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlite("Data Source=jwtSample.db");
+    }
+}
---
+++ JWTSample/JWTSample/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace JWTSample.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = "unnamed";
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
