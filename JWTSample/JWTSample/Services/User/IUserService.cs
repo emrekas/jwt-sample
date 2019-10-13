f5987ad65d5d14645a413c813686d7d31fd92809
Yunus Emre KAŞ <y.emrekas@outlook.com>
2019-10-13 23:46:36 +0300

    Servisler oluşturuldu.

---
+++ JWTSample/JWTSample/Services/User/IUserService.cs
@@ -0,0 +1,7 @@
+﻿namespace JWTSample.Services.User
+{
+    public interface IUserService
+    {
+        (string username, string token) Authenticate(string username, string password);
+    }
+}
---
+++ JWTSample/JWTSample/Services/User/UserService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace JWTSample.Services.User
+{
+    //DI için oluşturduğumuz arayüzü implemente ediyoruz
+    public class UserService : IUserService
+    {
+        public (string username, string token) Authenticate(string username, string password)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
